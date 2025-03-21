ab8cb3f93ae814a142d36161924c8f0b75a60ecf
btcthirst <btcthirst@gmail.com>
2025-03-21 21:57:13 +0200

    simple echo server

--- main.go
+++ main.go
@@ -1,142 +1,37 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
-	"github.com/cenkalti/backoff/v4"
-	"github.com/cockroachdb/cockroach-go/v2/crdb"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("point main 0")
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	db, err := initStore()
-	if err != nil {
-		log.Fatalf("failed to initialize the store: %s", err)
-	}
-	defer db.Close()
-	fmt.Println("point main 1")
-	e.GET("/", func(c echo.Context) error {
-		return rootHandler(db, c)
-	})
+	e.GET("/", healthCheckHandler)
 
 	e.GET("/ping", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
 
-	e.POST("/send", func(c echo.Context) error {
-		return sendHandler(db, c)
-	})
-
 	httpPort := os.Getenv("PORT")
 	fmt.Println("from os.getenv: ", httpPort)
 	if httpPort == "" {
-		httpPort = "8080"
+		httpPort = "4000"
 		fmt.Println("from =: ", httpPort)
 	}
 
 	e.Logger.Fatal(e.Start(":" + httpPort))
 }
 
-type Message struct {
-	Value string `json:"value"`
-}
-
-func initStore() (*sql.DB, error) {
-	fmt.Println("point init store 0")
-	pgConnString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
-		os.Getenv("PGHOST"),
-		os.Getenv("PGPORT"),
-		os.Getenv("PGDATABASE"),
-		os.Getenv("PGUSER"),
-		os.Getenv("PGPASSWORD"),
-	)
-
-	var (
-		db  *sql.DB
-		err error
-	)
-	fmt.Println("point init store 1")
-	openDB := func() error {
-		db, err = sql.Open("postgres", pgConnString)
-		return err
-	}
-	fmt.Println("point init store 2")
-	err = backoff.Retry(openDB, backoff.NewExponentialBackOff())
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("point init store 3")
-	if _, err := db.Exec(
-		"CREATE TABLE IF NOT EXISTS message (value TEXT PRIMARY KEY)"); err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func rootHandler(db *sql.DB, c echo.Context) error {
-	fmt.Println("point root h")
-	r, err := countRecords(db)
-	if err != nil {
-		return c.HTML(http.StatusInternalServerError, err.Error())
-	}
-	return c.HTML(http.StatusOK, fmt.Sprintf("Hello, Docker! (%d)\n", r))
-}
-
-func sendHandler(db *sql.DB, c echo.Context) error {
-	fmt.Println("point send h")
-	m := &Message{}
-
-	if err := c.Bind(m); err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-
-	err := crdb.ExecuteTx(context.Background(), db, nil,
-		func(tx *sql.Tx) error {
-			_, err := tx.Exec(
-				"INSERT INTO message (value) VALUES ($1) ON CONFLICT (value) DO UPDATE SET value = excluded.value",
-				m.Value,
-			)
-			if err != nil {
-				return c.JSON(http.StatusInternalServerError, err)
-			}
-			return nil
-		})
-
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-
-	return c.JSON(http.StatusOK, m)
-}
-
-func countRecords(db *sql.DB) (int, error) {
-	fmt.Println("point count")
-	rows, err := db.Query("SELECT COUNT(*) FROM message")
-	if err != nil {
-		return 0, err
-	}
-	defer rows.Close()
-
-	count := 0
-	for rows.Next() {
-		if err := rows.Scan(&count); err != nil {
-			return 0, err
-		}
-		rows.Close()
-	}
-
-	return count, nil
+func healthCheckHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 }
